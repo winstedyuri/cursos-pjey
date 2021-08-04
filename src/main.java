a1fe567d1a854ed71c8a247c0f47ac59674f78cd
patrickfreiree <patrickfaed@gmail.com>
2021-08-03 22:46:36 -0300

    1 novo usuario, add os 3 getEmail no println.

--- src/main.java
+++ src/main.java
@@ -9,9 +9,11 @@ public class main {
         Scanner ler = new Scanner(System.in);
         Usuario u1 = new Usuario("yuri@nexti.com", "teste12345");
         Usuario u2 = new Usuario("felipe@felipe.com", "senhausuario2");
+        Usuario u3 = new Usuario("patrick@kcirtap.com", "4321" );
 
 
+        System.out.println(u2.getEmail() +" - "+ u1.getEmail() + " - "+ u3.getEmail());
         System.out.println(u2.getSenha()); //Aqui estou acessando o atributo senha da class usuario.
-        System.out.println(u2.getEmail());
+
     }
 }
